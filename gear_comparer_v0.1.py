1384c313bb7161d661ac3cbe1a24687a94105286
Dominik Sigl <dominik.sigl.01@gmail.com>
2025-12-15 20:24:28 +0100

    Change to 160 Essences

--- gear_comparer_v0.1.py
+++ gear_comparer_v0.1.py
@@ -1,20 +1,19 @@
 """Module creating a GUI to compare Gear from LOTRO"""
 
 
+import pickle
 import customtkinter as ctk
 
 
-ctk.set_default_color_theme('dark-blue') # "blue" (standard), "green", "dark-blue"
-ctk.set_appearance_mode('dark') # "system" (standard), "dark", "light"
 items = []
 essence_values = {
-    'vitality': 1776,
-    'primary': 1448,
-    'crit': 3068,
-    'mastery': 2487,
-    'finesse': 4092,
-    'mitigation': 2511,
-    'outheal': 7083
+    'vitality': 3185,
+    'primary': 2609, 
+    'crit': 5277,
+    'mastery': 4380,
+    'finesse': 6994,
+    'mitigation': 4221,
+    'outheal': 12102
 }
 classes = {
     # 0:Might, 1:Agility, 2:Will
@@ -106,18 +105,18 @@ class ItemFrame(ctk.CTkFrame):
 
     def calculate(self):
         """Calculates an item and displays it"""
-        might = int(0 if(x := self.might_entry.get())=='' else x)
-        agility = int(0 if(x := self.agility_entry.get())=='' else x)
-        will = int(0 if(x := self.will_entry.get())=='' else x)
+        might = int(0 if (x := self.might_entry.get())=='' else eval(safe_translate(x)))
+        agility = int(0 if(x := self.agility_entry.get())=='' else eval(safe_translate(x)))
+        will = int(0 if(x := self.will_entry.get())=='' else eval(safe_translate(x)))
         selected_class_multipliers = classes[nav_frame.class_select.get()]
-        result_vit = int(0 if (x := self.vitality_entry.get())=='' else x)/essence_values['vitality'] if nav_frame.vitality_checkbox.get() else 0
-        result_crit = (int(0 if (x := self.crit_entry.get())=='' else x)+[0][0]+selected_class_multipliers[0][0]*might+selected_class_multipliers[1][0]*agility+selected_class_multipliers[2][0]*will)/essence_values['crit'] if nav_frame.crit_checkbox.get() else 0
-        result_mastery = (int(0 if(x := self.mastery_entry.get())=='' else x)+selected_class_multipliers[0][2]*might+selected_class_multipliers[1][2]*agility+selected_class_multipliers[2][2]*will)/essence_values['mastery'] if nav_frame.mastery_checkbox.get() else 0
-        result_physmit = (int(0 if(x := self.physmit_entry.get())=='' else x)+selected_class_multipliers[0][4]*might+selected_class_multipliers[1][4]*agility+selected_class_multipliers[2][4]*will)/essence_values['mitigation'] if nav_frame.physmit_checkbox.get() else 0
-        result_tactmit = (int(0 if(x := self.tactmit_entry.get())=='' else x)+selected_class_multipliers[0][5]*might+selected_class_multipliers[1][5]*agility+selected_class_multipliers[2][5]*will)/essence_values['mitigation'] if nav_frame.tactmit_checkbox.get() else 0
-        result_ogh = (int(0 if(x := self.ogheal_entry.get())=='' else x)+selected_class_multipliers[0][3]*might+selected_class_multipliers[1][3]*agility+selected_class_multipliers[2][3]*will)/essence_values['outheal'] if nav_frame.ogheal_checkbox.get() else 0
-        result_finesse = (int(0 if(x := self.finesse_entry.get())=='' else x)+selected_class_multipliers[0][1]*might+selected_class_multipliers[1][1]*agility+selected_class_multipliers[2][1]*will)/essence_values['finesse'] if nav_frame.finesse_checkbox.get() else 0
-        result_slots = int(0 if(x := self.slot_entry.get())=='' else x) if nav_frame.slot_checkbox.get() else 0
+        result_vit = int(0 if (x := self.vitality_entry.get())=='' else eval(safe_translate(x)))/essence_values['vitality'] if nav_frame.vitality_checkbox.get() else 0
+        result_crit = (int(0 if (x := self.crit_entry.get())=='' else eval(safe_translate(x)))+selected_class_multipliers[0][0]*might+selected_class_multipliers[1][0]*agility+selected_class_multipliers[2][0]*will)/essence_values['crit'] if nav_frame.crit_checkbox.get() else 0
+        result_mastery = (int(0 if(x := self.mastery_entry.get())=='' else eval(safe_translate(x)))+selected_class_multipliers[0][2]*might+selected_class_multipliers[1][2]*agility+selected_class_multipliers[2][2]*will)/essence_values['mastery'] if nav_frame.mastery_checkbox.get() else 0
+        result_physmit = (int(0 if(x := self.physmit_entry.get())=='' else eval(safe_translate(x)))+selected_class_multipliers[0][4]*might+selected_class_multipliers[1][4]*agility+selected_class_multipliers[2][4]*will)/essence_values['mitigation'] if nav_frame.physmit_checkbox.get() else 0
+        result_tactmit = (int(0 if(x := self.tactmit_entry.get())=='' else eval(safe_translate(x)))+selected_class_multipliers[0][5]*might+selected_class_multipliers[1][5]*agility+selected_class_multipliers[2][5]*will)/essence_values['mitigation'] if nav_frame.tactmit_checkbox.get() else 0
+        result_ogh = (int(0 if(x := self.ogheal_entry.get())=='' else eval(safe_translate(x)))+selected_class_multipliers[0][3]*might+selected_class_multipliers[1][3]*agility+selected_class_multipliers[2][3]*will)/essence_values['outheal'] if nav_frame.ogheal_checkbox.get() else 0
+        result_finesse = (int(0 if(x := self.finesse_entry.get())=='' else eval(safe_translate(x)))+selected_class_multipliers[0][1]*might+selected_class_multipliers[1][1]*agility+selected_class_multipliers[2][1]*will)/essence_values['finesse'] if nav_frame.finesse_checkbox.get() else 0
+        result_slots = int(0 if(x := self.slot_entry.get())=='' else eval(safe_translate(x))) if nav_frame.slot_checkbox.get() else 0
         result = round(result_vit+result_crit+result_mastery+result_physmit+result_tactmit+result_ogh+result_finesse+result_slots, 2)
         self.result_label.configure(text=result, width=80)
 
@@ -127,6 +126,27 @@ class ItemFrame(ctk.CTkFrame):
         for i, item in enumerate(items):
             item.grid(row=i+1)
         self.destroy()
+    
+    def to_list(self):
+        """Converts CtkFrame to List"""
+        return [self.name_entry.get(), self.vitality_entry.get(), self.might_entry.get(), self.agility_entry.get(), self.will_entry.get(), self.crit_entry.get(), self.mastery_entry.get(), self.finesse_entry.get(), self.physmit_entry.get(), self.tactmit_entry.get(), self.ogheal_entry.get(), self.slot_entry.get(), self.result_label.cget('text')]
+    
+    def from_list(self, list):
+        """Fills this CTkFrame with data from List"""
+        if len(list[0])>0: self.name_entry.insert(0, list[0])
+        if len(list[1])>0: self.vitality_entry.insert(0, list[1])
+        if len(list[2])>0: self.might_entry.insert(0, list[2])
+        if len(list[3])>0: self.agility_entry.insert(0, list[3])
+        if len(list[4])>0: self.will_entry.insert(0, list[4])
+        if len(list[5])>0: self.crit_entry.insert(0, list[5])
+        if len(list[6])>0: self.mastery_entry.insert(0, list[6])
+        if len(list[7])>0: self.finesse_entry.insert(0, list[7])
+        if len(list[8])>0: self.physmit_entry.insert(0, list[8])
+        if len(list[9])>0: self.tactmit_entry.insert(0, list[9])
+        if len(list[10])>0: self.ogheal_entry.insert(0, list[10])
+        if len(list[11])>0: self.slot_entry.insert(0, list[11])
+        self.result_label.configure(text=list[12])
+        return self
 
 
 def add_item():
@@ -135,14 +155,39 @@ def add_item():
     new_item.grid(row=len(items), sticky='w')
     add_button.grid(row=len(items)+1)
 
+
+def on_close():
+    """Saves the state of the window"""
+    with open('save.pickle', 'wb') as f:
+        d = [l.to_list() for l in items]
+        pickle.dump(d, f, pickle.HIGHEST_PROTOCOL)
+    root.destroy()
+
+
+def safe_translate(string):
+    """Removes everthing from a string except numbers and +-*"""
+    return ''.join(c for c in string if c in '0123456789+-*')
+
+
+ctk.set_default_color_theme('dark-blue') # "blue" (standard), "green", "dark-blue"
+ctk.set_appearance_mode('dark') # "system" (standard), "dark", "light"
 root = ctk.CTk()
 root.geometry('1630x500')
 root.title('Gear Comparer')
 add_button = ctk.CTkButton(root, text='+', command=add_item)
 nav_frame = NavFrame(root)
-first_item = ItemFrame(root)
 nav_frame.grid(sticky='w')
-first_item.grid(row=1, sticky='w')
-add_button.grid(row=2)
-
+try:
+    with open('save.pickle', 'rb') as file:
+        data = list(pickle.load(file))
+        if len(data) == 0:
+            first_item = ItemFrame(root)
+            first_item.grid(row=1, sticky='w')
+        for l in data:
+            ItemFrame(root).from_list(l).grid(row=len(items), sticky='w')
+except Exception as e:
+    first_item = ItemFrame(root)
+    first_item.grid(row=1, sticky='w')
+add_button.grid(row=len(items)+1)
+root.protocol('WM_DELETE_WINDOW', on_close)
 root.mainloop()
